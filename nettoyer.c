4ec014046cfd6b96d22fc3598594f4e7bf79445d
baptistePrunot <ipodbat@gmail.com>
2016-11-03 09:54:50 +0100

    rangement bis

--- nettoyer.c
+++
@@ -1,7 +0,0 @@
-#include "nettoyer.h"
-
-void nettoyer(int cachette, int tailleSujet)
-{
-    int taille = lseek(cachette, SEEK_SET, SEEK_END) - tailleSujet - sizeof(int);
-    ftruncate(cachette, taille);
-}
